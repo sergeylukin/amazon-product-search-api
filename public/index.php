b442bd2dae8bf2cef6ca6aad6fefbeb4806d8107
Sergey Lukin <contact@sergeylukin.com>
2015-10-28 05:52:52 +0200

    Setup API routing with Slim framework

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php require __DIR__ . '/../vendor/autoload.php';
+
+use Sergeylukin\AmazonProductSearch as Amazon;
+
+$app = new \Slim\Slim();
+
+$app->get('/api/v1/products', function () use ($app) {
+
+  try {
+
+    $Product = Amazon::factory()->findOneProductByKeyword($app->request->get('keyword'));
+
+    // send response header for JSON content type
+    $app->response()->header('Content-Type', 'application/json');
+
+    echo json_encode((array)$Product);
+
+  } catch (Exception $e) {
+
+    $app->response()->status(400);
+    $app->response()->header('X-Status-Reason', $e->getMessage());
+
+  }
+
+});
+
+$app->run();
